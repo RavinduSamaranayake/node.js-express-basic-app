379611d4130beffaaee2537244fb4f6700658292
RavinduSamaranayake <ovkravindu95@gmail.com>
2019-01-31 13:23:33 +0530

    working with rendering views

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
-//console.log('Hello Node');
 const express = require('express');
-const app = express();
+const path = require('path');
 const port = 3000;
 
-app.get('/',function(req,res){ 
-    res.send('Hello World.............')});
+//initialize app
+const app = express();
 
+//load view engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', `pug`);
+
+
+//Home route
+app.get('/',function(req,res){ 
+   // res.send('Hello World.............')});
+   res.render('index'); //view index.pug
+});
+
+
+//Start server    
 app.listen(port, function(){
-    console.log(`Example app listening on port ${port}!`)}); // this `` is very important.. this '' is not working
+    console.log(`Example app listening on port ${port}!`);
+});
