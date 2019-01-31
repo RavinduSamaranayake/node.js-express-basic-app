ec723f1132d0075b5dfbe87551b48cd5b9adc4e2
RavinduSamaranayake <ovkravindu95@gmail.com>
2019-01-31 21:05:16 +0530

    complete basics with pugs

--- app.js
+++ app.js
@@ -12,9 +12,42 @@ app.set('view engine', `pug`);
 
 //Home route
 app.get('/',function(req,res){ 
-   // res.send('Hello World.............')});
-   res.render('index'); //view index.pug
-});
+
+   let articles = [{id:1,                                //define associative array
+                    title:'article one',
+                    author:'kushan ravindu',
+                    body:'this is article 1'},
+
+                    {id:2,
+                     title:'article two',
+                     author:'kushan ravindu',
+                     body:'this is article 2'},
+
+                    {id:3,
+                     title:'article three',
+                     author:'kushan ravindu',
+                     body:'this is article 3'}
+                        
+                    
+                ];
+
+
+   res.render('index',{
+        //pass value to word
+         word:'Article',
+         articles: articles
+   });
+}); 
+
+//Add route
+app.get('/articles/add',function(req,res){ 
+    // res.send('Hello World.............')});
+    res.render('add_articles',{
+         //pass value to word
+          word:'Add Articles'
+    });
+ });
+
 
 
 //Start server    
